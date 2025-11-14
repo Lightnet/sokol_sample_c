e99936ec616d5e581926c8b48524fbe9dbb35fd9
lightnet <darkneter@gmail.com>
2025-11-13 21:03:13 -0800

    test script.

---
+++ script.lua
@@ -0,0 +1,7 @@
+-- script.lua  –  optional user script
+print("Lua script loaded!")
+
+-- You could define your own hello_world here, but the C side already
+-- provides one, so we just call it from the button.
+
+hello_world()
